4ce4433ca18e58981d48b45bff5e91d77e9f764e
hicham khaleq <hichamkhaleq6@gmail.com>
2024-12-25 15:57:43 +0100

    les classes:Role et Personne

--- src/Personne.java
+++ src/Personne.java
@@ -1,3 +1,17 @@
 public class Personne {
+    private int id;
+    private String nom;
+    private int age;
 
+    public Personne(int id, String nom, int age) {
+        this.id = id;
+        this.nom = nom;
+        this.age = age;
+    }
+
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
 }
---
+++ src/Rôle.java
@@ -0,0 +1,12 @@
+public class Rôle {
+    private int id;
+    private String nom;
+
+    public Rôle(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+}
